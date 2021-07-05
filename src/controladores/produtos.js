4eafc8c0417f010b33902e66b24ea43b7fd2e5b7
Everton santos <everton0102@gmail.com>
2021-07-05 16:19:00 -0300

    Endpoint ok

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -3,14 +3,30 @@ const { lerArquivo } = require('../utilitarios/db');
 
 
 async function listarProdrutos( req, res){
-    const arquivo = await lerArquivo();
+    let {produtos} = await lerArquivo();
+    const { categoria, precoInicial, precoFinal } = req.query;
 
 
 
-    console.log(arquivo);
+    if(!produtos){
+        return res.status(200).json([])
+    }
 
-    
+     produtos = produtos.filter(prod => prod.estoque > 0);
 
+    if(categoria){
+        produtos = produtos.filter(prod => prod.categoria.toLowerCase() === categoria.toLowerCase());
+    }
+
+    if(precoInicial){
+        produtos = produtos.filter(prod => prod.preco >= precoInicial);
+    }
+    if(precoFinal){
+        produtos = produtos.filter(prod => prod.preco <= precoFinal);
+    }
+
+    return res.status(200).json(produtos);
+   
 }
 
 
